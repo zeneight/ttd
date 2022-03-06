46d2855601ca4ae2235bce330adbca39362ad979
zeneight <thezeneight@gmail.com>
2022-03-06 12:22:50 +0800

    feat: change logo, settings and ui

--- public/js/app.js
+++ public/js/app.js
@@ -79723,8 +79723,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/zeneight/apps/others/ttd/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/zeneight/apps/others/ttd/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/zeneight/Apps/projects/small/ttd/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/zeneight/Apps/projects/small/ttd/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
