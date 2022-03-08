3f0b569958bd514b2886968dc61bb71062c0150e
zeneight <thezeneight@gmail.com>
2022-03-08 18:59:18 +0800

    feat: add category migration

---
+++ database/migrations/2022_03_08_104944_create_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('judul');
+            $table->string('deskripsi')->nullable();
+            $table->string('halaman');
+            $table->string('kordinat_x');
+            $table->string('kordinat_y');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
