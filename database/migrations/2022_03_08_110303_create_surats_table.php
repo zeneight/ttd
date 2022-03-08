9776af1110f5a627a82c1459ff3aba2548a4efc5
zeneight <thezeneight@gmail.com>
2022-03-08 19:08:03 +0800

    feat: add surat migration

---
+++ database/migrations/2022_03_08_110303_create_surats_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuratsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surats', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('kat_id');
+            $table->foreign('kat_id')->references('id')->on('categories');
+            $table->string('judul');
+            
+            $table->string('file_surat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('surats');
+    }
+}
