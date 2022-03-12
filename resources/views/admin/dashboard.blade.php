ff0562056d5dc25f13e986e0fa9f9bb5e174b24f
Agung Priambada <thezeneight@gmail.com>
2022-03-12 18:13:48 +0800

    feat: add dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -11,13 +11,131 @@
     </div>
     <div class="col-md-12">
         @if($message = Session::get('success'))
-            <div class="alert alert-success  alert-dismissible">
-                <button type="button" class="close" data-dismiss="alert">×</button>  
-                <strong>{{ $message }}</strong>
-            </div>
+        <div class="alert alert-success  alert-dismissible">
+            <button type="button" class="close" data-dismiss="alert">×</button>
+            <strong>{{ $message }}</strong>
+        </div>
         @endif
 
-        TESTEST
+        <section class="content">
+
+        <div class="row">
+                <div class="col-lg-offset-4 col-lg-4 col-xs-12">
+
+                    <div class="small-box bg-aqua">
+                        <div class="inner">
+                            <h3>150</h3>
+                            <p>Total Surat</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-envelope-o"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                
+
+            </div>
+
+            <div class="row">
+                
+
+                <div class="col-lg-offset-3 col-lg-3 col-xs-6">
+
+                    <div class="small-box bg-green">
+                        <div class="inner">
+                            <h3>44</h3>
+                            <p>Sudah Di-tandatangani</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-envelope"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-xs-6">
+
+                    <div class="small-box bg-red">
+                        <div class="inner">
+                            <h3>65</h3>
+                            <p>Belum Di-tandatangani</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-envelope"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div class="box box-default color-palette-box">
+                <div class="box-header with-border">
+                    <h3 class="box-title"><i class="fa fa-tag"></i> Jenis Surat</h3>
+                </div>
+                <div class="box-body">
+                    <div class="row">
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+                            <div class="info-box">
+                                <span class="info-box-icon bg-aqua"><i class="fa fa-envelope-o"></i></span>
+                                <div class="info-box-content">
+                                    <span class="info-box-text">SKU</span>
+                                    <span class="info-box-number">1,410</span>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+                            <div class="info-box">
+                                <span class="info-box-icon bg-green"><i class="fa fa-flag-o"></i></span>
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Bookmarks</span>
+                                    <span class="info-box-number">410</span>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+                            <div class="info-box">
+                                <span class="info-box-icon bg-yellow"><i class="fa fa-files-o"></i></span>
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Uploads</span>
+                                    <span class="info-box-number">13,648</span>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+                            <div class="info-box">
+                                <span class="info-box-icon bg-red"><i class="fa fa-star-o"></i></span>
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Likes</span>
+                                    <span class="info-box-number">93,139</span>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+
+        </section>
 
     </div>
 </div>
